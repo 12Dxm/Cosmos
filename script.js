55661b5673abc719a086dbd0027f26d9009cc717
twizzy <139641473+12Dxm@users.noreply.github.com>
2024-04-07 20:28:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let result = document.getElementById("result")
+let check = document.getElementById("checkButton")
+        
+    check.addEventListener('click', function() {
+      result.style.display === "block";
+});
